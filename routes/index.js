0b27fa2efd72b8eef57e3298d8be5331ceda6488
antonymca <antomca@gmail.com>
2020-01-11 20:05:38 -0500

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -7,7 +7,7 @@ router.get('/', function(req, res, next) {
   const ipInfo = req.headers.host;
   logger.info(JSON.stringify(ipInfo));
   logger.info('entered base url');
-  res.json({"data":[{"name":"antony"}]});
+  res.json({"data":[{"name":"user1"}]});
 });
 
 module.exports = router;
