b908d0f44d7603bec753c9501dc174e5525368fb
Antony <antomca@gmail.com>
2020-01-12 19:39:03 -0500

    updated response

--- routes/index.js
+++ routes/index.js
@@ -7,7 +7,7 @@ router.get('/', function(req, res, next) {
   const ipInfo = req.headers.host;
   logger.info(JSON.stringify(ipInfo));
   logger.info('entered base url');
-  res.json({"data":[{"name":"user1"}]});
+  res.json({"data":[{"name":"user8"}]});
 });
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -7,7 +7,7 @@ router.get('/', function(req, res, next) {
   const ipInfo = req.headers.host;
   logger.info(JSON.stringify(ipInfo));
   logger.info("entered the users url");
-  res.json({"data":[{"name":"user1"},{"name":"user2"},{"name":"user3"}]});
+  res.json({"data":[{"name":"user4"},{"name":"user5"},{"name":"user6"}]});
 });
 
 module.exports = router;
